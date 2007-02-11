1bd41b297014745b3bbef60d55efecfdf753efda
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-02-11 07:43:35 +0000

    More tweak. Mostly comments.

--- src/js/options.js
+++ src/js/options.js
@@ -9,32 +9,49 @@
  * @param {Function} f >>> Typically an annonymous function 
  * @return{Function} >>> Returns f with added options object 
  */
-// EXAMPLE
+
 /*
+// EXAMPLE
+// should be able to do this...
+var func = function f() {
+	var opts = f.options.temp(tempOpts);
+	// do stuff using opts...
+	alert(opts.ding + " " + opts.ring);
+};
+func.options({
+	ding: "dong", 
+	ring: "wrong"
+)};
+//But according to Crockford, you can't access a function properties like this in IE. 
+// Works in Firefox though
+// But we can do this.
 var func = function () {
-	var defaultOpts = {ding: "dong", ring: "wrong"};
 	var f = function (tempOpts) {
 		var opts = f.options.temp(tempOpts);
 		// do stuff using opts...
 		alert(opts.ding + " " + opts.ring);
-	};	
-	return f.options(defaultOpts);
+	};
+	return f.options({
+		ding: "dong", 
+		ring: "wrong"
+	});
 }();
-Options for func can now be used in three ways:
-	1) Use default options: 
+//Options for func can now be used in three ways:
+	//1) Use default options: 
 		func(); //alerts "dong wrong"
-	2) Non-destructively use one or more different options until func.options.reset() is called:
+	//2) Non-destructively use one or more different options until func.options.reset() is called:
 		func.options.set({ding: "bling"});
 		func(); // now alerts "bling wrong"
 		func.options.reset(); 
 		func(); // alerts "dong wrong"
-	3) Use options for one call only:
+	//3) Use options for one call only:
 		func({ding: "bling"}); // alerts "bling wrong"
 		func(); // alerts "dong wrong"		
 */
-// Call executeOnce right now and assign the result to options
+
+// Call an annonymous function right now and assign the result to Function.prototype.options
 // Calling options creates a new options object
-Function.prototype.options = function once() {
+Function.prototype.options = function () {
 	// Create prototype for options objects
 	var optsProto = {
 		_defaultOpts: {},
@@ -51,8 +68,10 @@ Function.prototype.options = function once() {
 		}	
 	};
 	
-	// Return a function and assign it to options
-	// (This gets executed only once)
+	// f.options({...}) creates options for a function and returns f.
+	// f.options() overrides Function.prototype.options(), replacing it with an object.
+	// This means that for each function, f.options() can only be called once...
+	// ...unless you do this first: delete f.options;
 	return function (defaultOpts) {
 		this.options = deepProto(optsProto); 
 		this.options._defaultOpts = defaultOpts; // Ideally, this would be hidden
@@ -60,18 +79,7 @@ Function.prototype.options = function once() {
 		return this;
 	}	
 }();
-// Example of use of options objects
-var func = function () {
-	var f = function (tempOpts) {
-		var opts = f.options.temp(tempOpts);
-		// do stuff using opts...
-		alert(opts.ding + " " + opts.ring);
-	};
-	return f.options({
-		ding: "dong", 
-		ring: "wrong"
-	});
-}();
+
 // proto() is an exact copy of Douglass Crockford's object() function (Mochit's clone() function is very similar)
 // Create a new object and set its prototype property to o.
 function proto(o) {
