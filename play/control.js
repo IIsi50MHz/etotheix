b9828f06f63f7b89b130fef340f698796ebf857c
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-03-24 02:07:27 +0000

    Cleaned up comments.

--- play/control.js
+++ play/control.js
@@ -8,65 +8,89 @@ function proto(o) {
 	F.prototype = o;
 	return new F();
 }
-
+// The protoControl object
+// With the help of this object, you can add simple html markup defining the behavior of a control or a whole class of controls.
+//	If you define controls (or a class of controls) on elements created dynamically, don't worry, they'll still work fine. 
+//	This is because the event handlers are not normally bound to the control element; instead, they are bound to the parent 
+//	element of the control definition markup.
 /*
-** Need to explain how the use the protoDelegator better. Below are some comments I wrote while trying to figure out what I was doing.
-control.html might have some useful comments and expamples.
-	Need to include
-		- Purpose of protoDelegator
-		- Primary uses
-			create controls
-			link elements to controls in html
-		- Examples of use
+How to create a control:
+	javascript code: 
+		// Call on page load.
+		protoControl.init();
+	markup:
+		<!-- Use markup like this to define the behavior for a control or a class of controls. -->
+			<!-- NOTE: -->
+			<!-- If the definition markup is for a class of controls, they must be contained within the parent element of the definition -->
+			<!-- If the parent element of a control definition is destroyed, you're screwed... -->
+			<!-- ...You can, however, create and destroy the control elements themselves at will. -->
+			
+		<!-- EXAMPLE 1: Setting up behavior for a class of controls -->		
+		<div id="controlContainerBlah">
+			<!-- this span cotains a control definition -->
+			<span control=".ding">  <!-- the control attribute refers to the control elements -->
+				<!-- these anchors define how the control affects other elements -->
+				<!-- the hook attribute refers an element affected by the control -->
+				<!-- the event and action attributes define when and how the elements are affected  -->				
+				<a hook=".ding" event="click" action="select"></a> <!-- when a class ding control is clicked, select it... -->
+				<a hook="#dong" event="click" action="toggleShow"></a> <!-- when a class ding control is clicked, show or hide the element with id="dong" -->
+			</span>			
+			<ul id="classDingControls"> 				
+				<li class="ding">do</li>
+				<li class="ding">re</li>
+				<li class="ding">mi</li>
+				<li class="ding">fa</li>
+				<li class="ding">so</li>
+				<!-- if you dynamically add more class ding controls here -- say, "la" and "ti" -- they will just work -->
+			</ul>
+		</div>
+		<h1 id="dong">DONG!</h1>
+			
+		<!-- EXAMPLE 2: Setting up behavior for a stand-alone control -->
+		<h1 id="boo">BOO!
+			<!-- when the "boo" control is clicked, select it and show or hide the element with id="dong" -->
+			<span control="#boo"> 
+				<a hook="#boo" event="click" action="select"></a>
+				<a hook="#eek" event="click" action="toggleShow"></a>
+			</span>						
+		</h1>
+		<p id="eek">EEK!</p>
+
+How to create custom actions that override default actions:
+	javascript code:
+		protoControl.extend({
+			// overrides default select action for class "ding" elements only
+			".ding select": function () {
+				alert("Don't do default select for .ding controls. Pop up this alert instead.")
+			},
+			// creates a new default select action for all elements
+			select: function () {
+				alert("Pop up this alert instead for all ")
+			}
+		});
+	markup:
+		<!-- markup is same as before, but now clicking on a ding element sends up an alert -->
+		<span control=".ding">
+			<a hook=".ding" event="click" action="select"></a>
+			<a hook="#dong" event="click" action="toggleShow"></a>
+		</span>	
 */
 
-/*
-Selector (tabs or buttons are examples of selectors) (mongoose event list, mongoose target list, trainee tabs, etc.) ** would "Control" be a better name?
-	A selector element can effect one (or more?) elements when an event happens
-	What sort of mark-up should we use to indicate what elements a selector element affects?
-		- use href = id of affected element. That means selector would have to be an anchor. Do we want that?
-		- have one or more hidden anchors inside selector element that refer to affected elements
-		- href value could be a jQuery expression? (css selector, xpath)
-	Use event delegation for potentially transient selectors, or where there are multiple selectors present that can use the same event handler.
-*/
-
-/*
-DELEGATOR OBJECT **Not sure this is a very goode name. But may expand functionality of this object so it makes more sense.
-Wraps event delegating functionality around an element
-Use this when you have a bunch of selectable controls with internal structure and you want to delagate event handling to some higher level container.
-The element that does the event delegation should contain the selectable elements and nothing else. (**maybe, or maybe not)
-**Need to add some functionality to this object
-	way to bind one or more event types to delegator. Or maybe just one event type. Should create a new object for each event type event if delegator element is the same?
-	way to identify the lowest level container that contains the (potentially composite) elements to delegate to
-	way to identify the (potentially composite) elements that need to be delagated to function that handles event. Maybe an array of functions?
-Example:	
-	var ding = makeDelegator("#delegator").container("#container").control(".ding");
-	ding.bind("click", function(e) {
-		//do stuff
-		ding.selectOne();
-		alert(ding.e.target);
-		alert(e.target);
-	});
-	...
-	// by default control is same as delegator
-*/
-
-var protoDelegator = {
+var protoControl = {
 	// jQuery object holding the delegator element. Events are bound to the delegator element.
 	_delegator: "",
 	// jQuery expression that identifies the control element(s),
 	_control: "", 
 	
 	// holds the moset recent event - updated by event()
-	e: "",	// Should this be private -- provide an acces function?
+	e: "",	// **Should this be private -- provide an acces function?
 	// holds the target of the most recent event - updated by event()
 	eventTarget: "",
 	
 	// holds the most recent target control (jQuery object) - updated by event()
-	targetControl: null, //**Should this be private -- provide a function to access?
+	targetControl: null, // **Should this be private -- provide a function to access?
 	// holds other controls other controls that are similar to target control (jQuery object) - updated by event()
-	otherControls: null, //**Should this be private -- provide a function to access?
-	
+	otherControls: null, // **Should this be private -- provide a function to access?	
 	// sets event e and calculates targetControl element
 	event: function (e) {
 		// don't bother doing anything unless we've got a new event		
@@ -88,7 +112,8 @@ var protoDelegator = {
 			}
 		}
 		return this;
-	},	
+	},
+	
 	// binds a function to the delegator element
 	bind: function (type, data, func) {
 		var that = this;
@@ -102,21 +127,20 @@ var protoDelegator = {
 		});
 		return this;
 	},
-
-	// initializes all delegator elements inside context node (default is document)
+	// initializes all delegator controls that are within the context node (default is document)
 	init: function (context) {
-		//gather all control elements 
-		var controls = jQ("div[@control]", context || document);
+		// gather all control definitions
+		var controlDefs = jQ("span[@control]", context || document);
 		// set up event handlers for each control
-		controls.each(function () {			
-			var control = jQ(this);
-			var delegatorObj = protoDelegator.make(control.parent(), control.attr("control"));
+		controlDefs.each(function () {
+			var controlDef = jQ(this);
+			var delegatorObj = protoControl.make(controlDef.parent(), controlDef.attr("control"));
 			// get all the control anchors inside this control
 			var controlAnchors = jQ("a", this);
-			// for each control anchor, hook up the appropriate event handler to the delegator element 
+			// for each control anchor, hook up the appropriate event handler to the delegator element
 			controlAnchors.each(function () {
-				var controlAnchor = jQ(this);				
-				var hook = controlAnchor.attr("hook") || delegatorObj._control;				
+				var controlAnchor = jQ(this);
+				var hook = controlAnchor.attr("hook") || delegatorObj._control;	
 				var event = controlAnchor.attr("event") || "click";	//**user should be able set default event and action for page to something else		
 				var action = controlAnchor.attr("action") || "select";
 				var funcName;
@@ -136,6 +160,7 @@ var protoDelegator = {
 				});
 			});			
 		});
+		return this;
 	},
 	// makes a new delegator object
 	// the delegator argument can be an id selector (example: "#ding"), an element, or a jQuery wrapped element
@@ -149,24 +174,11 @@ var protoDelegator = {
 	// extends delegator with custom actions
 	// These custom functions can overwrite the default actions...
 	// ...or they can override the default actions for specific controls or elements
-	/*
-	// EXAMPLE:
-		protoDelegtor.extend({
-			// selecting the element with id="ding" now sends up an alert after it is selected
-			"#ding select": function (expr) {
-				this.select(expr);
-				alert("Ding!");
-			},
-			// default deselect doesn't deleselet anything any more--alerts "blah" insead.
-			deselect: function () {
-				alert("blah");
-			}				
-		})
-	*/
 	extend: function (obj) {
 		for (var i in obj) {
 			this[i] = obj[i];
 		}
+		return this;
 	},
 	/////
 	// Default Actions
@@ -187,14 +199,14 @@ var protoDelegator = {
 		return this;
 	},
 	// Select the target control and deselect the other controls. Selecting the same control twice in a row does nothing.
-	selectOne: function () {
+	selectOne: function () { //** Should accept and arument... Should be able to use on a group of elements other than the control.
 		// select new element
 		this.select(this.targetControl);
 		// deselect old elements
 		this.deselect(this.otherControls.filter(".selected"));
 		return this;
 	},
-	// Select the target control and deselect the other controls. Selecting the same control twice in a row toggles the control.
+	// Select the target control and deselect the other controls. Selecting the same control twice in a row toggles the
 	toggleSelectOne: function () {
 		// toggle target control
 		this.toggleSelect(this.targetControl);
@@ -202,21 +214,23 @@ var protoDelegator = {
 		this.deselect(this.otherControls.filter(".selected"));
 		return this;
 	},
-	show: function () {},
-	hide: function () {},
-	toggleShow: function () {}
+	show: function (expr) {
+		jQ(expr).show();
+	},
+	hide: function () {
+		jQ(expr).hide();
+	},
+	toggleShow: function (expr) {
+		jQ(expr).toggle();
+	}
 }
 
-protoDelegator.extend({
+protoControl.extend({
 	//".thing selectOne": function () {this.selectOne(); alert('hello');},
 	//"#ding select": function (expr) {this.select(expr); alert("ding!");},
 	//"#ding show": function () {}
 });
 
-/////////////////////// 
-// "onload" 
-//   **Seems like there's might be too much going on here. A lot of the code here could probably be used other places. 
-//	Need write some general purpose functions/objects and use them instead.
 jQ(function () {
-	protoDelegator.init();
+	protoControl.init();
 })
