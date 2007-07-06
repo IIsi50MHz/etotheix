003c4a15f05d0c3ec98ac9d63408193a42c19699
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-07-06 19:35:49 +0000

    multiple versions of jObject in this one

--- trunk/src/js/jObject.js
+++ trunk/src/js/jObject.js
@@ -160,7 +160,7 @@ var jObject = (function () {
 		// create a new jObject object
 		var new_jObject = proto(jObject.prototype);		
 		new_jObject.obj = typeof obj === "object" ? obj : {};
-		return  function (x, y) {		
+		var f = function (x, y) {		
 			xIsObject = typeof x === "object";
 			xIsString = typeof x === "string";						
 			xIsDefined = !!x;
@@ -186,7 +186,9 @@ var jObject = (function () {
 			} else {
 				return new_jObject;
 			}
-		};	
+		};
+		f.obj = new_jObject.obj;		
+		return f;
 	};
 	// add fuctionality to jObject and Object.prototypejObject
 	jObject.augment = function (x) {
@@ -218,3 +220,111 @@ var jObject = (function () {
 		}
 	};
 })();
+
+////////// Another attempt
+var ding = {};
+var dong = {};
+var blah = jObject(ding); // creates a new jObject; returns a function
+blah(dong); // augments the ding object
+blah()
+
+var jObject = (function ()  {
+	// helper function
+	each(obj, f) {
+		for (var i in obj) {
+		for (var i in obj) {
+			if (typeof obj === "function") {
+				if (Function.prototype[i] === undefined) {
+					f(obj[i]);
+				}
+			} else {
+				if (Array.prototype[i] === undefined && Object.prototype[i] === undefined) {
+					f(obj[i]);
+				}
+			}
+		}
+		return obj;
+	}
+	// helper function for extending objects
+	var extend = function (o, obj) {
+		return each(obj, function () {
+			o[i] = obj[i];	
+		})		
+	};
+	// public function for augmenting jObjects
+	var jObject.augment = function (obj) {		
+		for (var i in obj) {
+			if (typeof obj[i] === "function") {				
+				// bind all functions to this.obj
+				augment_jObject[i] = function () {
+					obj[i].apply(this.obj, arguments);
+					return this;
+				}
+			} else {
+				augment_jObject[i] = obj[i];				
+			}
+		}
+		return jObject.augment;
+	};	
+	// function for extending a jObject's wrapped object
+	var extend_jObject = function (jObj, obj) {
+		extend(jObj.obj, obj);
+		return jObj;
+	};
+	
+	// return a function that creates the jObject
+	return function (obj) {		
+		// Create a new jObject
+		// A jObject is a function returns the wrapped object
+		var new_jObject = function (obj) {
+			obj = obj || {};			
+			extend_jObject(new_jObject, obj);
+			return new_jObject.obj;
+		}	
+		// Add add wrapped object to new jObject
+		new_jObject.obj = obj;
+		// Add jObject functionality ot new jObject
+		extend(new_jObject, jObject.augment);
+		
+		return new_jObject;	
+	};	
+})();
+
+jObject.augment({
+	proto: function () {
+		var F = function F() {};
+		F.prototype = this;
+		var obj = new F();		
+	},		
+	extend: function (obj) {
+		for (var i in obj) {
+			this[i] = obj[i];
+		}
+		return this;
+	},
+	copy: function () {
+		return extend({}, this);
+	},
+	clear: function () {
+		for (var i in this) {
+			delete this[i];
+		}
+		return this;
+	}, 
+	each: function (f) {
+		for (var i in this) {
+			if (typeof this === "object") {
+				if (Array.prototype[i] === undefined && Object.prototype[i] === undefined) {
+					f(this[i]);
+				}				
+			} else if (typeof this === "function") {
+				if (Function.prototype[i] === undefined) {
+					f(this[i]);
+				}
+			}
+		}
+		return this;
+	}
+});
+
+
