b116b57187fcad593208bd88a9e591c1d5c80ee8
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-07-07 05:24:09 +0000

    - Made some changes to structure jObject. - Fixed some bugs. - Added jObject.html for testing jObject.

--- src/js/jObject.js
+++ src/js/jObject.js
@@ -1,9 +1,6 @@
 var jObject;
 (function () {
-	/////
-	// helper functions	
-	/////
-	
+	// typeof functions
 	function isObject(x) {
 		return x !== null && typeof x === "object";
 	}
@@ -11,50 +8,37 @@ var jObject;
 		return typeof x === "function";
 	}
 	function isBasic(x) {
-		return typeof x === "string" || typeof x === "number" || typeof x === "boolean" || typeof x === "undefined" || typeof x === null || typeof x !== typeof x;
+		return !(isObject(x) || isFunction(x));			
 	}
-	// Function for iterating over objects
-	function each(obj, f) {		
-		var thisObj = obj;		
-		for (var i in thisObj) {
-			if (isObject(thisObj)) {
-				if (Array.prototype[i] === undefined && Object.prototype[i] === undefined) {
-					f(i, thisObj[i]);
-				}				
-			} else if (isFunction(thisObj)) {
-				if (Function.prototype[i] === undefined) {
-					f(i, thisObj[i]);
-				}
-			}						
-		}
-		return this;
-	}
-	// Functions extending objects
-	function extend(o, obj) {		
-		each (obj, function (key, val) {
-			o[key] = val;
-		})
-		return o;
-	}
-	function deepExtend(o, obj) {
-		for (var i in obj) {
-			if (isObject(obj[i])) {	
-				if (!isObject(o[i])) {				
-					o[i] = {};
-				}
-				deepExtend(o[i], obj[i]);	
-			} else {
-				o[i] = obj[i];
-			}
-		}
-		return o;
-	}
-	// Functions creating new objects
+	// shallow functions	
 	function proto(obj) {
 		var F = function F() {};
 		F.prototype = obj;
 		return new F();
 	}
+	function update(o, obj) {
+		each (obj, function (key, val) {
+			o[key] = val;
+		});		
+		return o;
+	}
+	function clear(obj) {
+		for (var i in obj) {
+			delete obj[i];
+		}
+		return update(this.obj, obj || {});
+	}
+	function each(obj, f) {		
+		for (var i in obj) {
+			if (!isBasic(obj)) {
+				if (Array.prototype[i] === undefined && Function.prototype[i] === undefined) {
+					f(i, obj[i]);
+				}				
+			}					
+		}
+		return obj;
+	}
+	// deep functions
 	function deepProto(obj) {
 	    var o = proto(obj);
 	    for (var i in obj) {
@@ -64,85 +48,102 @@ var jObject;
 	    }
 	    return o;
 	}
+	function deepUpdate(o, obj) {		
+		for (var i in obj) {
+			//console.debug("obj is object?", obj[i], isObject(obj[i]))
+			if (isObject(obj[i])) {	
+				//console.debug("obj is !basic?", o[i], !isBasic(o[i]));
+				if (isBasic(o[i])) {				
+					o[i] = {};
+				}
+				deepUpdate(o[i], obj[i]);
+			} else {
+				o[i] = obj[i];
+			}
+		}
+		return o;
+	}
+	function deepClear(obj) {
+		for (var i in obj) {
+			if (!isObject(obj[i])) { 
+				delete obj[i];
+			} else {
+				deepClear(obj[i]);
+			}	
+		}
+		return obj;
+	}
+	dc = deepClear;
+	function deepEach(obj, f) {
+		each(obj, function (key, val) {
+			if (!isBasic(val)) {
+				deepEach(val, f);	
+			} 
+		});
+		return obj;
+	}	
 	// The jObject function returns a function that wraps extra functionality around obj
 	jObject = function (obj) {
 		// create a new jObject		
-		var new_jObject = function (obj) {
-			obj = obj || {};
-			extend(new_jObject.obj, obj);
-			return new_jObject.obj;
-		};
+		var new_jObject = proto(obj);
 		// add add wrapped object to new jObject
-		new_jObject.obj = obj;		
+		new_jObject.obj = obj;
 		// add jObject functionality ot new jObject
-		extend(new_jObject, jObject.augment);		
+		update(new_jObject, jObject.augment);		
 		return new_jObject;			
 	};	
 	
 	jObject.each = each;
-	jObject.extend = extend;
+	jObject.update = update;
 	
 	// This function allows you to add functionality to all subsequently created jObjects
 	jObject.augment = function (obj) {				
-		extend(jObject.augment, obj);					
+		update(jObject.augment, obj);					
 	};
 	
 	// add basic functionality to jObject
 	jObject.augment({
 		// shallow functions
-		proto: function () {
+		proto: function (obj) {
 			var F = function F() {};
 			F.prototype = this.obj;
-			return jObject(new F());
+			return jObject(new F()).update(obj || {});
 		},
-		extend: function (obj) {		
-			extend(this.obj, obj)
+		update: function (obj) {		
+			update(this.obj, obj || {});
 			return this;
 		},
 		copy: function (obj) {
-			var copy = extend({}, this.obj);
-			return jObject(extend(copy, obj));
+			var copy = update({}, this.obj);
+			return jObject(copy).update(obj || {});
 		},
 		clear: function (obj) {
-			var thisObj = this.obj;
-			for (var i in thisObj) {
-				delete thisObj[i];
-			}
-			return this.extend(obj || {});
+			clear(this.obj);
+			return update(this.obj, obj || {});
 		}, 			
 		each: function (f) {
 			each(this.obj, f);
 			return this;
 		},
 		//deep functions
-		deepProto: function () {			
-			return jObject(deepProto(this.obj));
+		deepProto: function (obj) {			
+			return jObject(deepProto(this.obj)).deepUpdate(obj);
 		},
-		deepExtend: function (obj) {
-			deepExtend(this.obj, obj);
+		deepUpdate: function (obj) {
+			deepUpdate(this.obj, obj);
 			return this;
 		},
 		deepCopy: function (obj) {
-			var copy = deepExtend({}, this.obj);					
-			return jObject(deepExtend(copy, obj));			
+			var copy = deepUpdate({}, this.obj);					
+			return jObject(copy).deepUpdate(obj);		
 		},
-		deepClear: function () {
-			var thisObj = this.obj;
-			for (var i in thisObj) {
-				if (typeof thisObj[i] === 'object') { 
-					obj[i] = this.deepClear(thisObj[i]);
-				}
-				delete thisObj[i];
-			}
-			return this.deepExtend(obj || {});
+		deepClear: function (obj) {
+			deepClear(this.obj);
+			return this.deepUpdate(obj || {});
 		},
 		deepEach: function (f) {
-			var thisObj = this.obj;
-			each(thisObj, function (key, val) {
-				if (typeof val === 'object') {
-					deepEach(val, f);	
-				}
-			})			
+			deepEach(this.obj, f);
+			return this;
 		}
 	});
 })();
