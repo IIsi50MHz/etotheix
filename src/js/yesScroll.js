53de329b7587036ca77a89a990fc9f6870f0d5f1
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-08-09 06:29:15 +0000

    started noScroll.js and yesScroll.js bookmarklets

---
+++ src/js/yesScroll.js
@@ -0,0 +1,5 @@
+// load jQuery
+var s = document.createElement('script');
+s.setAttribute('src', 'http://jquery.com/src/jquery-latest.js');
+document.getElementsByTagName('body')[0].appendChild(s);
+console.debug(jQuery("a"));
