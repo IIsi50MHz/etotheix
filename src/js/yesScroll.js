b16a6eccde521350cbd9a7562198803f49e2a1de
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2007-08-09 06:32:46 +0000

    working on noScroll.js and yesScroll.js bookmarklets

--- src/js/yesScroll.js
+++ src/js/yesScroll.js
@@ -1,5 +1,7 @@
 // load jQuery
 var s = document.createElement('script');
 s.setAttribute('src', 'http://jquery.com/src/jquery-latest.js');
-document.getElementsByTagName('body')[0].appendChild(s);
-console.debug(jQuery("a"));
+s.onload = function () {
+	document.getElementsByTagName('body')[0].appendChild(s);
+	console.debug(jQuery("a"));
+}
