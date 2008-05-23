e0e08397a8fd49ac0fb50d8c255f11184a752733
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-05-23 21:40:22 +0000

    working on timesheet.js

--- src/js/timesheet.js
+++ src/js/timesheet.js
@@ -1,16 +1,17 @@
-var hoursId = "TG_TOTALHOURS_";
-var earningsCodeId = "txtTG_PAYCODEID_";
-var departmentId = "txtTG_DEPARTMENTID_";
-var jobId = "txtTG_WORKEDJOBID_";
+// laad jQuery
+setTimeout(function () {
+	jQuery.noConflict();
+	(function ($) {
+		var hoursId = "#TG_TOTALHOURS_";
+		var earningsCodeId = "#txtTG_PAYCODEID_";
+		var departmentId = "#txtTG_DEPARTMENTID_";
+		var jobId = "#txtTG_WORKEDJOBID_";
 		
-for (var i = 4; i <= 12; i += 2) {
-	var hours = document.getElementById(hoursId+i);	
-	var earningsCode = document.getElementById(departmentId+i);
-	var department = document.getElementById(jobId+i);
-	
-	hours.value = "8.00";
-	earningsCode.value = "100011";
-	department.value = "AIS-ENG-1422";	
-	
-	hours.onchange();	
-}	
+		for (var i = 4; i <= 12; i += 2) {
+			$(hoursId+i).val(8.00).focus().change().blur();		
+			$(departmentId+i).val(100011).focus().change().blur();	
+			$(jobId+i).val("AIS-ENG-1422").focus().change().blur();	
+		}	
+	})(jQuery);
+}, 4000)
+
