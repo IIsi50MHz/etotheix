3b010485dc49e079f8383418bff98731f048e999
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-05-23 21:24:54 +0000

    working on timesheet.js

--- trunk/src/js/timesheet.js
+++ trunk/src/js/timesheet.js
@@ -1,13 +1,17 @@
 // laad jQuery
-(function ($) {
-	var hoursId = "#TG_TOTALHOURS_";
-	var earningsCodeId = "#txtTG_PAYCODEID_";
-	var departmentId = "#txtTG_DEPARTMENTID_";
-	var jobId = "#txtTG_WORKEDJOBID_";
-	
-	for (var i = 4; i <= 12; i += 2) {
-		$(hoursId+i).val(8.00).focus().change().blur();		
-		$(departmentId+i).val(100011).focus().change().blur();	
-		$(jobId+i).val("AIS-ENG-1422").focus().change().blur();	
-	}	
-})(jQuery);
+setTimeout(function () {
+	jQuery.noConflict();
+	(function ($) {
+		var hoursId = "#TG_TOTALHOURS_";
+		var earningsCodeId = "#txtTG_PAYCODEID_";
+		var departmentId = "#txtTG_DEPARTMENTID_";
+		var jobId = "#txtTG_WORKEDJOBID_";
+		
+		for (var i = 4; i <= 12; i += 2) {
+			$(hoursId+i).val(8.00).focus().change().blur();		
+			$(departmentId+i).val(100011).focus().change().blur();	
+			$(jobId+i).val("AIS-ENG-1422").focus().change().blur();	
+		}	
+	})(jQuery);
+}, 4000)
+
