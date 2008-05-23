d05704150e1609f1befd194aaeb03360e879d2d8
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-05-23 20:17:03 +0000

    timesheet.js!

---
+++ trunk/src/js/timesheet.js
@@ -0,0 +1,6 @@
+// laad jQuery
+var s=document.createElement('script');
+s.setAttribute('src', 'http://jquery.com/src/jquery-latest.js');
+document.getElementsByTagName('body')[0].appendChild(s);
+
+console.debug("heeloo!")
