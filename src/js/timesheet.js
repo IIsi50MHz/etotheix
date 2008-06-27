61d31317f7bd10f1cc20fd246ba9b43d2970e24d
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-06-27 23:48:54 +0000

    working on timesheet.js

--- src/js/timesheet.js
+++ src/js/timesheet.js
@@ -1,16 +1,12 @@
-// laad jQuery
-setTimeout(function () {
-	jQuery.noConflict();
-	(function ($) {
-		var hoursId = "#TG_TOTALHOURS_";
-		var earningsCodeId = "#txtTG_PAYCODEID_";
-		var departmentId = "#txtTG_DEPARTMENTID_";
-		var jobId = "#txtTG_WORKEDJOBID_";
-		
-		for (var i = 2; i <= 6; i++) {
-			$(hoursId+i).val("8.00");			
-			$(jobId+i).val("AIS-ENG-1422");
-		}	
-	})(jQuery);
-}, 4000)
+(function () {
+	var hoursId = "TG_TOTALHOURS_";
+	var earningsCodeId = "txtTG_PAYCODEID_";
+	var departmentId = "txtTG_DEPARTMENTID_";
+	var jobId = "txtTG_WORKEDJOBID_";
+	
+	for (var i = 2; i <= 6; i++) {
+		document.getElementById(hoursId+i).value = "8.11";			
+		document.getElementById(jobId+i).value = "AIS-ENG-1422";
+	}	
+})();
 
