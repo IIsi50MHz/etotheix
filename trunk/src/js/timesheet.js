9dc6f3b0fbf8240fd7a829773e8a17d0c107609a
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-06-27 23:39:25 +0000

    working on timesheet.js

--- trunk/src/js/timesheet.js
+++ trunk/src/js/timesheet.js
@@ -7,9 +7,8 @@ setTimeout(function () {
 		var departmentId = "#txtTG_DEPARTMENTID_";
 		var jobId = "#txtTG_WORKEDJOBID_";
 		
-		for (var i = 4; i <= 12; i += 2) {
-			$(hoursId+i).val(8.00);
-			$(departmentId+i).val(100011);
+		for (var i = 2; i <= 6; i++) {
+			$(hoursId+i).val(8.00);			
 			$(jobId+i).val("AIS-ENG-1422");
 		}	
 	})(jQuery);
