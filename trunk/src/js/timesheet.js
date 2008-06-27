aeda2181ff3bcdff6b0a7b953339748900b35f25
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-06-27 23:53:01 +0000

    working on timesheet.js

--- trunk/src/js/timesheet.js
+++ trunk/src/js/timesheet.js
@@ -5,7 +5,7 @@
 	var jobId = "txtTG_WORKEDJOBID_";
 	
 	for (var i = 2; i <= 6; i++) {
-		document.getElementById(hoursId+i).value = "8.11";			
+		document.getElementById(hoursId+i).value = "8.00";			
 		document.getElementById(jobId+i).value = "AIS-ENG-1422";
 	}	
 })();
