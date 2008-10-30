d37c5e029dd173a42fccd4e69577a65e05c93a92
bwrobinett <bwrobinett@fcfeb4b7-d628-0410-96d3-37939acd40c7>
2008-10-30 02:14:15 +0000

    1) adding images for window sizing for mongoose dev 2) adding changes made ages ago that never got checked in

--- play/delegate.js
+++ play/delegate.js
@@ -1,7 +1,7 @@
-/* 
-delegate plugin for jQuery
-*/
-(function (jQuery, selectorMap) {
+/////
+// delegate plugin for jQuery
+/////
+(function ($, selectorMap) {
 	// local variables
 	selectorMap = {" ": "parents", ">": "parent", "+": "prev", "~": "prevSiblings"};
 	// local functions **need to comment these better
@@ -35,7 +35,39 @@ delegate plugin for jQuery
 		var e = document.createEvent(options.eventTypeType);		
 		e.initEvent(eventType, options.canBubble, options.cancelable);	
 		elem.dispatchEvent(e);		
-	}	
+	}
+	function triggerKeyEvent(elem, keyCode, options) { //**this probably works in firefox only
+		//f.debug("trigger1", keyCode, options);
+		options = $.extend({
+			eventType: "keypress", //  in DOMString typeArg,                                                           
+			canBubble: true,       //  in boolean canBubbleArg,                                                        
+			cancelable: true,      //  in boolean cancelableArg,                                                       
+			viewArg: null,         //  in nsIDOMAbstractView viewArg,  Specifies UIEvent.view. This value may be null.     
+			ctrlKeyArg: false,     //  in boolean ctrlKeyArg,                                                               
+			altKeyArg: false,      //  in boolean altKeyArg,                                                        
+			shiftKeyArg: false,    //  in boolean shiftKeyArg,                                                      
+			metaKeyArg: false,     //  in boolean metaKeyArg,                                                       
+			keyCodeArg: 0,   //  in unsigned long keyCodeArg,        
+			charCodeArg: keyCode   //  in unsigned long charCodeArg
+		}, options);
+		//f.debug("trigger2", keyCode, options);
+		var e = document.createEvent("KeyboardEvent");
+		//f.debug("trigger3", e, keyCode, options);
+		// create a key event
+		e.initKeyEvent(                                                                                      
+			options.eventType,    //  in DOMString typeArg,                                                           
+			options.canBubble,    //  in boolean canBubbleArg,                                                        
+			options.cancelable,   //  in boolean cancelableArg,                                                       
+			options.viewArg,      //  in nsIDOMAbstractView viewArg,  Specifies UIEvent.view. This value may be null.     
+			options.ctrlKeyArg,   //  in boolean ctrlKeyArg,                                                               
+			options.altKeyArg,    //  in boolean altKeyArg,                                                        
+			options.shiftKeyArg,  //  in boolean shiftKeyArg,                                                      
+			options.metaKeyArg,   //  in boolean metaKeyArg,                                                       
+			0, //  in unsigned long keyCodeArg,                                                      
+			keyCode   //  in unsigned long charCodeArg
+		);  		
+		elem.dispatchEvent(e);		
+	}
 	// extending jQuery
 	$.fn.extend({
 		// jQuery's filter() function only works with simple selectors. complexFilter() works with selectors likw this: "a, div > .blah, .ding + .dong"
@@ -50,7 +82,7 @@ delegate plugin for jQuery
 			this.each(function () {			
 				selector = selector || "*";			
 				siblings = $(this).parent().children();			
-				results = results.add(siblings.slice(0, siblings.index(this)).filter(selector));						
+				results = results.add(siblings.lt(siblings.index(this)).filter(selector));						
 			});
 			return results;
 		}, 		
@@ -76,14 +108,30 @@ delegate plugin for jQuery
 					}
 				}
 			});
-			return this;
+			return $(this);
 		},
 		// for triggering custom events that bubble. **don't try to use this with mouse or keyboard events yet! custom events only for now!
-		dispatch: function (eventType) { //**this probably doesn't work in IE ****It shouldn't be to hard to make this work with IE... See Flanagan's Javascript book.			
+		dispatch: function (eventType, callback) { //**this probably doesn't work in IE ****It shouldn't be to hard to make this work with IE... See Flanagan's Javascript book.						
 			this.each(function () {				
-				trigger(this, eventType);			
+				trigger(this, eventType);				
+				if (callback) callback();
+				
 			})
+			return this;
+		},
+		dispatchKeypress: function (keyChar, options) { //**this probably doesn't work in IE ****It shouldn't be to hard to make this work with IE... See Flanagan's Javascript book.												
+			//f.debug("keyp1");
+			options = options || {};
+			//f.debug("keyp1.1", options, options.useCode, keyChar);
+			var keyCode = options.useCode ? keyChar : keyChar.charCodeAt(0);			
+			//f.debug("keyp2");
+			this.each(function () {				
+				//f.debug("keyp2.1");
+				triggerKeyEvent(this, keyCode, options);
+				//f.debug("keyp2.2");
+			});
+			//f.debug("keyp3");
 			return this;
 		}
 	});	
-})($);
+})(jQuery);
